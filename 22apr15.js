58256fd060cc58bc8fb5089faac2a23d18a64d94
Christian Hicks <r.ca.hicks@gmail.com>
2022-04-15 10:52:07 -0400

    add 22apr15 coding challenge

---
+++ 22apr15.js
@@ -0,0 +1,10 @@
+//Given a string of two words, return the string with the first word placed after the second one.
+
+
+
+
+
+
+function nameShuffler(str){
+  return str.split(' ').reverse().join(' ');
+}
