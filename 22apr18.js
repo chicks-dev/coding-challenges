ba669c312bdc48ea0738943876f9801349fbe4c9
Christian Hicks <r.ca.hicks@gmail.com>
2022-04-18 11:32:36 -0400

    add 22apr18 coding challenge

---
+++ 22apr18.js
@@ -0,0 +1,15 @@
+//Make two functions that take in an array of numbers and return the smallest and largest number respectively
+
+
+
+
+
+var min = function(list){
+
+    return Math.min(...list);
+}
+
+var max = function(list){
+
+    return Math.max(...list);
+}
